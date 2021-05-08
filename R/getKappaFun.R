3bce5341e1b435a100c2ced922ee1c21022eee73
yc_ozdemir <ysfcn1998@gmail.com>
2021-05-08 07:46:22 +0300

    deneme deneme

--- R/getKappaFun.R
+++ R/getKappaFun.R
@@ -25,7 +25,7 @@ getKappaFun <- function(tTarget, Q, D, guess, ftol,
   #'
   #' @importFrom nlsr nlxb
 
-  #' @author Selçuk Korkmaz, \email{selcukorkmaz@gmail.com}
+  #' @author Selcuk KORKMAZ, \email{selcukorkmaz@gmail.com}
   #'
   #' @seealso \code{\link{SEIQRDP}} \code{\link{fit_SEIQRDP}}
   #'
